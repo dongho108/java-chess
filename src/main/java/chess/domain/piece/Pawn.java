b87f8b8c3beaa9e4b45451c5455675daa9f5573c
dongho <dongho108@naver.com>
2022-03-25 23:53:00 +0900

    style: 쓸모없는 주석 제거

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -9,22 +9,4 @@ public final class Pawn extends Piece {
     public Pawn(Color color) {
         super(color, NAME, new StartedPawn(color.forward()));
     }
-
-    // private Pawn(Color color, State state) {
-    //     super(color, NAME, state);
-    //     // 1 : 0
-    //     // true : false
-    //     // Enum
-    //     // Color (1, true);
-    //     // Black (Direction.Down)
-    //     // forward = color.getDirection()
-    // }
-
-    // public static Pawn of(Color color) {
-    //     return new Pawn(color)
-        // if (color == Color.White) {
-        //     return new Pawn(color, new StartedWhitePawn());
-        // }
-        // return new Pawn(color, new StartedBlackPawn());
-    // }
 }
