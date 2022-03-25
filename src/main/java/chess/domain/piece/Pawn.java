e5f78d9387d5c7561d5168949314c1a101876124
dongho <dongho108@naver.com>
2022-03-25 23:49:07 +0900

    feat: 폰 행마법 구현

--- src/main/java/chess/domain/piece/Pawn.java
+++ src/main/java/chess/domain/piece/Pawn.java
@@ -1,12 +1,30 @@
 package chess.domain.piece;
 
 import chess.domain.piece.property.Color;
-import chess.domain.piece.state.StartedPawn;
+import chess.domain.piece.state.started.StartedPawn;
 
 public final class Pawn extends Piece {
     private static final String NAME = "p";
 
     public Pawn(Color color) {
-        super(color, NAME, new StartedPawn());
+        super(color, NAME, new StartedPawn(color.forward()));
     }
+
+    // private Pawn(Color color, State state) {
+    //     super(color, NAME, state);
+    //     // 1 : 0
+    //     // true : false
+    //     // Enum
+    //     // Color (1, true);
+    //     // Black (Direction.Down)
+    //     // forward = color.getDirection()
+    // }
+
+    // public static Pawn of(Color color) {
+    //     return new Pawn(color)
+        // if (color == Color.White) {
+        //     return new Pawn(color, new StartedWhitePawn());
+        // }
+        // return new Pawn(color, new StartedBlackPawn());
+    // }
 }
